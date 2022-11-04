38e06ca0f17d172b89a7b1189bfc7c30e98c5c43
DyQ3HELOF1 <87905975+DyQ3HELOF1@users.noreply.github.com>
2022-11-04 20:24:26 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'euler-two';
+export class AppComponent implements OnInit {
+
+  private fibLow: number;
+  private fibHigh: number;
+  private intermediateSum: number;
+  public result: number;
+
+  ngOnInit(): void {
+    this.initializeDefaults();
+    let nextFib = this.calculateNextFib(this.fibLow, this.fibHigh);
+    while (nextFib <= 4000000) {
+      this.intermediateSum += nextFib;
+      this.fibLow = this.fibHigh;
+      this.fibHigh = nextFib;
+      nextFib = this.calculateNextFib(this.fibLow, this.fibHigh);
+    }
+    this.result = this.intermediateSum;
+  }
+
+  private initializeDefaults(): void {
+    this.fibLow = 2;
+    this.fibHigh = 8;
+    this.intermediateSum = 10;
+  }
+
+  private calculateNextFib(fibLow: number, fibHigh: number): number {
+    return 4 * fibHigh + fibLow;
+  }
 }
